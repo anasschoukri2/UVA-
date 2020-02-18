618e48e0d823c9cb70b5e67415913a27e4567496
anass CHOUKRI <38406227+anasschoukri2@users.noreply.github.com>
2020-02-18 11:59:35 +0100

    Update 10227.cpp

--- 10227.cpp
+++ 10227.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-int sett[40005];
+#include<bits/stdc++.h>
+#define MAXN 10000
+
+using namespace std;int sett[40005];
 int findd(int a)
 {
 	if (a == sett[a]) return a;
@@ -13,38 +14,40 @@ void join(int a, int b)
 	if (a == b) return;
 	sett[b] = a;
 }
-int main()
-{
-	int n, a, b;
-	cin >> n;
-	while (n--)
-	{
-		memset(sett, -1, sizeof(sett));
-		int p, t;
-		cin >> p >> t;
-		string u;
-		cin.ignore();
-		vector<int> opin[330];
-		while (getline(cin, u))
-		{
-			int a = 0, b = 0;
-			int i = 0;
-			for (i = 0; u[i] != ' '; i++)
-				a = a *10 + (u[i] - '0');
-			i++;
-			for (; i < u.size(); i++)
-				b = b *10 + (u[i] - '0');
-			opin[a].push_back(b);
-			sett[a] = a;
-		}
-		for (int i = 1; i <= p; i++)
-			for (int j = 1; j <= p; j++)
-				if (opin[i] == opin[j])
-					join(a, b);
-		int ans = 0;
-		for (int i = 1; i <= p; i++)
-			if (sett[i] == i)
-				ans++;
-		cout << ans << endl;
-	}
+int main(){
+    int y;
+    cin>>y;
+    while(y--){
+        string u;
+        int p,t;
+        for(int i=0;i<=300;i++)sett[i]=i;
+        cin>>p>>t;
+        set<int>op[307];
+        cin.ignore();
+        
+        while(getline(cin,u)&&u!="" ){
+
+            int a=0,b=0,i=0;
+
+            for(i=0;u[i]!=' ';i++){
+                a=a*10+(u[i]-'0');
+            }
+            for(i=i+1;i<u.size();i++){
+                b=b*10+(u[i]-'0');
+            }
+            
+            op[a].insert(b);
+        }
+        for(int i=1;i<=p;i++){
+            for(int j=1;j<=p;j++){
+                if(op[i]==op[j])join(i,j);
+            }
+        }
+        int ans=0;
+        for(int i=1;i<=p;i++)if(sett[i]==i)ans++;
+        cout<<ans<<endl;
+        if(y)cout<<endl;
+
+    }
+
 }
